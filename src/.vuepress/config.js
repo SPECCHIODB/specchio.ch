ff1e11eea2f2ab6b6dff1c3dc6653775c8f432fa
Ralph Huwiler <ralph@huwiler.rocks>
2019-02-05 14:27:30 +0100

    use sizes instead of size

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -16,9 +16,9 @@ module.exports = {
   ],
 
   head: [
-    ['link', { rel: 'apple-touch-icon', size: '180x180', href: `/icons/apple-touch-icon.png` }],
-    ['link', { rel: 'icon', type: 'image/png', size: '32x32', href: `/icons/favicon-32x32.png` }],
-    ['link', { rel: 'icon', type: 'image/png', size: '16x16', href: `/icons/favicon-16x16.png` }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: `/icons/apple-touch-icon.png` }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: `/icons/favicon-32x32.png` }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: `/icons/favicon-16x16.png` }],
     ['link', { rel: 'manifest', href: `/icons/site.webmanifest` }],
     ['link', { rel: 'mask-icon', color: '#5bbad5', href: `/icons/safari-pinned-tab.svg` }],
     ['link', { rel: 'shortcut icon', href: `/icons/favicon.ico` }],
