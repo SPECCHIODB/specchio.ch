54ddcc8c17b49d35884c92faa1df6c605c182098
Ralph Huwiler <ralph@huwiler.rocks>
2019-02-28 19:08:55 +0100

    extend the default vuepress theme

--- src/.vuepress/theme/index.js
+++ src/.vuepress/theme/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 // Theme API.
 module.exports = (options, ctx) => ({
+  extend: '@vuepress/theme-default',
   plugins: [
     '@vuepress/active-header-links',
     '@vuepress/search'
